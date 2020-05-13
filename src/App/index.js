39ced0fb7ecfbee1b9f34e18d5e5a0217d47cd6f
Rafael Melón <info@rafaelmelon.com>
2020-05-13 18:09:08 +0200

    feat(app): set min folder app

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Films from "../Films";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
